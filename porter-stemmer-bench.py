f57c153af46d920279514d78f91fc4725e579809
Ross Harrison <rtharrison86@gmail.com>
2016-02-11 23:55:46 -0500

    benchmarking porter stemmer

---
+++ porter-stemmer-bench.py
@@ -0,0 +1,37 @@
+import nltk
+import re
+import os
+import time
+
+p = nltk.PorterStemmer()
+
+b = re.compile(r"\b\w+\b")
+tokens = b.findall(open("./text/de-bello-gallico.txt").read().lower())
+
+def test(tokens):
+    stems = []
+    print("call...")
+    for i in range(0, 10):
+        stems = []
+        print("loop...", i)
+        for t in tokens:
+            stems.append(p.stem(t))
+
+    return stems
+
+start = time.time()
+stems = test(tokens)
+end = time.time()
+
+print((end - start))
+
+outPath = "./text/stem-bello-gallico.txt"
+print("Outputting stems to: ", outPath)
+
+if os.path.isfile(outPath):
+    os.remove(outPath)
+outF = open(outPath, "a+")
+
+for s in stems:
+    outF.write(s)
+    outF.write("\n")
