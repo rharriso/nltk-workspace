60fbd48e8a5f21f7a59add4e8b188f2d88d905ae
Ross Harrison <rtharrison86@gmail.com>
2016-02-20 21:52:03 -0500

    without file write

--- porter-stemmer-bench.py
+++ porter-stemmer-bench.py
@@ -9,27 +9,13 @@ b = re.compile(r"\b\w+\b")
 tokens = b.findall(open("./text/de-bello-gallico.txt").read().lower())
 
 def test(tokens):
-    stems = []
     print("call...")
     for i in range(0, 10):
         print("loop...", i)
-        stems = [p.stem(t) for t in tokens]
-
-    return stems
+        [p.stem(t) for t in tokens]
 
 start = time.time()
-stems = test(tokens)
+test(tokens)
 end = time.time()
 
 print((end - start))
-
-outPath = "./text/stem-bello-gallico.txt"
-print("Outputting stems to: ", outPath)
-
-if os.path.isfile(outPath):
-    os.remove(outPath)
-outF = open(outPath, "a+")
-
-for s in stems:
-    outF.write(s)
-    outF.write("\n")
